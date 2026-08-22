3ba3160746cf4bb821fdcf2abce4a93bab9cc950
JCPG62 <jotacpg@gmail.com>
2026-08-22 14:27:17 -0300

    Update qrmanut-sw.js

--- qrmanut-sw.js
+++ qrmanut-sw.js
@@ -1,10 +1,10 @@
 /* ============================================================
  * QRManut USA - Service Worker
- * Version 7.6.2.1
+ * Version 7.6.2.2
  * ============================================================ */
 
 const CACHE_PREFIX = "qrmanut-usa-";
-const CACHE_NAME = "qrmanut-usa-v7.6.2.1";
+const CACHE_NAME = "qrmanut-usa-v7.6.2.2";
 
 const APP_SHELL = [
   "./",
